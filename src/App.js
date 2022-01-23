cfd57f5b05572cb2a2e716e0850516a50602f998
Imran Cinra <imrancinra3446@gmail.com>
2022-01-23 12:50:57 +0600

    Components Ready

--- src/App.js
+++ src/App.js
@@ -1,26 +1,18 @@
-import logo from './logo.svg';
+
 import './App.css';
+import MainComponent from './components/MainComponents';
+import Header from './components/header/Header';
+import Body from './components/body/Body';
+import Footer from './components/footer/Footer';
+
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-
-          Learn React
-        </a>
-        <h1>Lorem ipsum dolor sit amet.</h1>
-        <h1>Lorem ipsum dolor sit amet.</h1>
-      </header>
+      <MainComponent />
+      <Header/>
+      <Body/>
+      <Footer/>
     </div>
   );
 }
---
+++ src/components/MainComponents.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import Header from './components/header/Header';
+import Body from './components/body/Body';
+import Footer from './components/footer/Footer';
+const MainComponent = () => {
+    return (
+        <div>
+            <Header />
+            <Body />
+            <Footer />
+        </div>
+    );
+}
+export default MainComponent;
